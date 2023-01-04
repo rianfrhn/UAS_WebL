d8029804e235e7dc34c3bcc18194a64634459626
Rian <48545316+rianfrhn@users.noreply.github.com>
2023-01-04 11:32:40 +0700

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -2,15 +2,15 @@ create database if not exists uasWebL;
 use uasWebL;
 
 create or replace table jadwal(
-	id int AUTO_INCREMENT,
-    hari varchar,
-    slot_waktu varchar,
-    dosen varchar,
-    ruang varchar,
-    mata_kuliah varchar,
-    tahun_ajaran int,
-    semester int,
-    primary key(id)
+	id int(11) not null AUTO_INCREMENT,
+ 	hari varchar(255),
+	slot_waktu varchar(255),
+    	dosen varchar(255),
+    	ruang varchar(255),
+    	mata_kuliah varchar(255),
+    	tahun_ajaran int(11),
+    	semester int(11),
+    	primary key(id)
 );
 create or replace table users(
 	id int(11) NOT NULL AUTO_INCREMENT,
